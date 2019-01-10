5d02c407cba9e2847aa73bcdc99191463dd092f3
Brad Howard <bhoward4@ewu.edu>
2019-01-10 15:10:51 -0800

    First Unit Test

---
+++ SecretSanta/src/SecretSanta.Domain/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretSanta.Domain.Models
+{
+    public class User
+    {
+        public string first { get; set; }
+        public string last { get; set; }
+
+        public User(string fName, string lName)
+        {
+            first = fName;
+            last = lName;
+        }
+    }
+}
---
+++ SecretSanta/test/SecretSanta.Domain.Tests/Class1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SecretSanta.Domain.Models;
+
+namespace SecretSanta.Domain.Tests
+{
+    [TestClass]
+    public class Class1
+    {
+        [TestMethod]
+        public void MakeUser()
+        {
+            User testUser = new User("Miles", "Prower");
+            Assert.AreEqual("Miles", testUser.first);
+        }
+    }
+}
