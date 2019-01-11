dda348c3277244a2ae3ccb8bd6dffa7462c1bdd3
Michael Stokesbary <mike@stokesbary.me>
2019-01-11 09:43:00 -0800

    Getting a basic entity model created

---
+++ SecretSanta/src/SecretSanta.Domain/Services/UserService.cs
@@ -0,0 +1,22 @@
+﻿using SecretSanta.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretSanta.Domain.Services
+{
+    public class UserService
+    {
+        private ApplicationDbContext DbContext { get; }
+        public UserService(ApplicationDbContext context)
+        {
+            DbContext = context;
+        }
+
+        public void AddUser(User user)
+        {
+            DbContext.Users.Add(user);
+            DbContext.SaveChanges();
+        }
+    }
+}
