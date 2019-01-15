bb253ac807d70d6d9a3a506422008f4aab79f194
Brad Howard <bhoward4@ewu.edu>
2019-01-14 19:43:16 -0800

    Added Gift Service
    The last Service added.
    TODO:: Unit Tests.

--- SecretSanta/src/SecretSanta.Domain/Services/GiftService.cs
+++ SecretSanta/src/SecretSanta.Domain/Services/GiftService.cs
@@ -15,5 +15,47 @@ namespace SecretSanta.Domain.Services
         }
 
         // TODO: Create, Edit, Delete gifts for a given User.
+        public void CreateGift(User user, Gift gift)
+        {
+            DbContext.Gifts.Add(gift);
+
+            if(!user.ListOfGifts.Contains(gift))
+            {
+                user.ListOfGifts.Add(gift);
+            }
+
+            DbContext.SaveChanges();
+        }
+
+        public void EditGift(User user, Gift gift)
+        {
+            if (gift.ID != default(int))
+                DbContext.Gifts.Update(gift);
+            else
+                DbContext.Gifts.Add(gift);
+
+            List<Gift> set = (List<Gift>)user.ListOfGifts;
+
+            for(int index = 0; index < set.Count; index++)
+            {
+                if(set[index].Title == gift.Title)
+                {
+                    set[index] = gift;
+                    break;
+                }
+            }
+
+            user.ListOfGifts = set;
+            DbContext.SaveChanges();
+        }
+
+        public void DeleteGift(User user, Gift gift)
+        {
+            DbContext.Gifts.Remove(gift);
+
+            user.ListOfGifts.Remove(gift);
+
+            DbContext.SaveChanges();
+        }
     }
 }
