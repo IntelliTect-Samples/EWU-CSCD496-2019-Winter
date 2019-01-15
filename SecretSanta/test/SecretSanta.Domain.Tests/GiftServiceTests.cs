83d8a30ac0a5b78e0a575ec8023306e5fa822478
Brad Howard <bhoward4@ewu.edu>
2019-01-15 15:44:58 -0800

    Unit Test for all Services
    Add all Unit tests, now need to fix models/services to pass the tests.

--- SecretSanta/test/SecretSanta.Domain.Tests/GiftServiceTests.cs
+++ SecretSanta/test/SecretSanta.Domain.Tests/GiftServiceTests.cs
@@ -65,5 +65,87 @@ namespace SecretSanta.Domain.Tests
                 Assert.AreEqual(true, giftService.HasGift(user, gift));
             }
         }
+
+        [TestMethod]
+        public void EditGift()
+        {
+            GiftService giftService;
+
+            User user = new User() { First = "Brad", Last = "Howard" };
+
+            Gift gift = new Gift()
+            {
+                Title = "Toaster Oven",
+                Description = "Toaster",
+                URL = "toasters.co.su",
+                WantTier = 999,
+                WhoWantIt = user
+            };
+
+            using (var context = new SecretSantaDbContext(Options))
+            {
+                giftService = new GiftService(context);
+
+                giftService.CreateGift(user, gift);
+            }
+
+            using (var context = new SecretSantaDbContext(Options))
+            {
+                giftService = new GiftService(context);
+
+                gift.URL = "amazon.com/Toaster/";
+
+                giftService.EditGift(user, gift);
+            }
+
+            using (var context = new SecretSantaDbContext(Options))
+            {
+                giftService = new GiftService(context);
+
+                if(giftService.HasGift(user, gift))
+                {
+                    List<Gift> giftList = (List<Gift>)user.Gifts;
+                    Assert.AreEqual("amazon.com/Toaster/", giftList[giftList.IndexOf(gift)]);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void DeleteGift()
+        {
+            GiftService giftService;
+
+            User user = new User() { First = "Brad", Last = "Howard" };
+
+            Gift gift = new Gift()
+            {
+                Title = "Toaster Oven",
+                Description = "Toaster",
+                URL = "toasters.co.su",
+                WantTier = 999,
+                WhoWantIt = user
+            };
+
+            using (var context = new SecretSantaDbContext(Options))
+            {
+                giftService = new GiftService(context);
+
+                giftService.CreateGift(user, gift);
+            }
+
+            using (var context = new SecretSantaDbContext(Options))
+            {
+                giftService = new GiftService(context);
+
+                giftService.DeleteGift(user, gift);
+            }
+
+            using (var context = new SecretSantaDbContext(Options))
+            {
+                giftService = new GiftService(context);
+
+                Assert.AreEqual(false, giftService.HasGift(user, gift));
+            }
+        }
     }
 }
