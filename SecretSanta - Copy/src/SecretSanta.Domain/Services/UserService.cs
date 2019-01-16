1c15dd1366735ccd0c831aa158683222a987c31f
ConnerVerret <cverret@eagles.ewu.edu>
2019-01-15 23:24:45 -0800

    Add user and gift

---
+++ SecretSanta - Copy/src/SecretSanta.Domain/Services/UserService.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SecretSanta.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SecretSanta.Domain.Services
+{
+    public class UserService
+    {
+        private ApplicationDbContext DbContext { get; set; }
+        public UserService(ApplicationDbContext dbContext)
+        {
+            DbContext = dbContext;
+        }
+
+        public User UpsertUser(User user)
+        {
+            if(user.Id == 0)
+            {
+                DbContext.Users.Add(user);
+            }
+            else
+            {
+                DbContext.Users.Update(user);
+            }
+
+            DbContext.SaveChanges();
+            return user;
+        }
+
+        public User Find(int id)
+        {
+            return DbContext.Users.Include(u => u.Gifts).SingleOrDefault(u => u.Id == id);
+        }
+
+        public List<User> FetchAll()
+        {
+            var userTask = DbContext.Users.ToListAsync();
+            userTask.Wait();
+
+            return userTask.Result;
+        }
+    }
+}
---
+++ SecretSanta/src/SecretSanta.Domain/Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretSanta.Domain.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Gift> Gifts { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    }
+}
