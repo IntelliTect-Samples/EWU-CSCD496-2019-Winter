10df884698cb138e2f5d8fa60931ec6497903896
Brad Howard <bhoward4@ewu.edu>
2019-01-20 19:41:53 -0800

    First series of test for Import services
    File open and read test are set, now need to work on parsing functionality

---
+++ SecretSanta/src/SecretSanta.Import/ImportService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Microsoft.Win32.SafeHandles;
+
+namespace SecretSanta.Import
+{
+    public class ImportService
+    {
+        public FileStream Istream { get; set; }
+
+        public ImportService(string path)
+        {
+            Istream = new FileStream(path, FileMode.Open, FileAccess.Read);
+        }
+
+        public string ReadName()
+        {
+            if(!Istream.CanRead)
+            {
+                throw new IOException("File Handling Error");
+            }
+
+            StreamReader streamReader = new StreamReader(Istream);
+            string name = streamReader.ReadLine();
+
+            return name;
+        }
+    }
+}
---
+++ SecretSanta/test/SecretSanta.Domain.Tests/ImportServiceTest.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SecretSanta.Domain.Models;
+using SecretSanta.Import;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretSanta.Domain.Tests
+{
+    [TestClass]
+    public class ImportServiceTest
+    {
+        private SqliteConnection SqliteConnection { get; set; }
+        private DbContextOptions<SecretSantaDbContext> Options { get; set; }
+
+        [TestInitialize]
+        public void OpenConnection()
+        {
+            SqliteConnection = new SqliteConnection("DataSource=:memory:");
+            SqliteConnection.Open();
+
+            Options = new DbContextOptionsBuilder<SecretSantaDbContext>().UseSqlite(SqliteConnection).Options;
+
+            using (var context = new SecretSantaDbContext(Options))
+            {
+                context.Database.EnsureCreated();
+            }
+        }
+
+        [TestCleanup]
+        public void CloseConnection()
+        {
+            SqliteConnection.Close();
+        }
+
+        [TestMethod]
+        public void OpenFileForImport_CanRead()
+        {
+            ImportService importService = new ImportService(@"D:\CScD Class\CScD 496\Repo\EWU-CSCD496-2019-Winter\SecretSanta\src\SecretSanta.Domain\imputFile.txt");
+            
+            Assert.AreEqual<bool>(true, importService.Istream.CanRead);
+        }
+
+        [TestMethod]
+        public void ReadFileForImport_ReadName()
+        {
+            ImportService importService = new ImportService(@"D:\CScD Class\CScD 496\Repo\EWU-CSCD496-2019-Winter\SecretSanta\src\SecretSanta.Domain\imputFile.txt");
+
+            Assert.AreEqual<string>("Name: Brad Howard", importService.ReadName());
+        }
+    }
+}
