6b47523d33b102427fd1e22c9f1bbb61a7ebe546
Isaac <ibliss@eagles.ewu.edu>
2019-01-23 01:08:53 -0800

    Testing file creation and the ability to read from files

--- SecretSanta/UnitTestProject1/FileImportAndUseTests.cs
+++ SecretSanta/UnitTestProject1/FileImportAndUseTests.cs
@@ -9,16 +9,45 @@ namespace Assignment2FileLibrary.Tests
     [TestClass]
     public class FileImportAndUseTests
     {
+        private string Path = "test.txt";
+
         [TestInitialize]
         public void TestInitialize()
         {
-            File.Create("test.txt");
+            if (File.Exists(Path))
+            {
+                File.Delete(Path);
+            }
+            
         }
 
         [TestMethod]
-        public void BasicFileExistTests()
+        public void BasicFileExistTest()
         {
+            using (FileStream fs = File.Create(Path))
+            {}
             Assert.IsTrue(File.Exists("test.txt"));
         }
+
+        [TestMethod]
+        public void TestToSeeIfThereIsTextInTheFile()
+        {
+            using (FileStream fs = File.Create(Path))
+            {
+                Byte[] info = new UTF8Encoding(true).GetBytes("text in a file");
+                // Add some information to the file.
+                fs.Write(info, 0, info.Length);
+            }
+            using (StreamReader sr = File.OpenText(Path))
+            {
+                string s = sr.ReadLine();
+                Assert.AreEqual("text in a file", s);
+            }
+        }
+
+
+
+        
     }
+
 }
