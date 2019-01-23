955630290b2aa2d65962f07594eee0ed2778cad0
Brad Howard <bhoward4@ewu.edu>
2019-01-22 20:22:48 -0800

    Placing file in Temp Directory
    Placing file into the user's temp directory.

--- SecretSanta/test/SecretSanta.Domain.Tests/ImportServiceTest.cs
+++ SecretSanta/test/SecretSanta.Domain.Tests/ImportServiceTest.cs
@@ -11,33 +11,36 @@ namespace SecretSanta.Domain.Tests
     [TestClass]
     public class ImportServiceTest
     {
-        private SqliteConnection SqliteConnection { get; set; }
-        private DbContextOptions<SecretSantaDbContext> Options { get; set; }
-
-        [TestInitialize]
-        public void OpenConnection()
+       [TestInitialize]
+       public void MakeTestFiles()
         {
-            SqliteConnection = new SqliteConnection("DataSource=:memory:");
-            SqliteConnection.Open();
-
-            Options = new DbContextOptionsBuilder<SecretSantaDbContext>().UseSqlite(SqliteConnection).Options;
-
-            using (var context = new SecretSantaDbContext(Options))
-            {
-                context.Database.EnsureCreated();
-            }
+            BuildTestFile("Name: Brad Howard", "inputFile.txt");
+            BuildTestFile("Name: Howard, Brad", "inputFile2.txt");
+            BuildTestFile("Name:", "inputFile3.txt");
+            BuildTestFile("Name", "inputFile4.txt");
+            BuildTestFile("Name: Howard", "inputFile5.txt");
+            BuildTestFile("Name: Howard,", "inputFile6.txt");
+            BuildTestFile("Name:Howard,", "inputFile7.txt");
         }
 
-        [TestCleanup]
-        public void CloseConnection()
+        public void BuildTestFile(string input, string fileName)
         {
-            SqliteConnection.Close();
+            string path = System.IO.Path.GetTempPath() + @"\" + fileName;
+
+            if(!File.Exists(path))
+            {
+                using (StreamWriter ostream = File.CreateText(path))
+                {
+                    ostream.WriteLine(input);
+                    ostream.Close();
+                }
+            }
         }
 
         [TestMethod]
         public void OpenFileForImport_CanRead()
         {
-            string path = Path.Combine(System.Environment.CurrentDirectory + @"\..\..\..\", "inputFile.txt");
+            string path = Path.Combine(System.IO.Path.GetTempPath() + @"\", "inputFile.txt");
             ImportService importService = new ImportService(path);
             
             Assert.AreEqual<bool>(true, importService.Istream.CanRead);
@@ -46,84 +49,100 @@ namespace SecretSanta.Domain.Tests
         [TestMethod]
         public void ReadFileForImport_ReadName()
         {
-            string path = Path.Combine(System.Environment.CurrentDirectory + @"\..\..\..\", "inputFile.txt");
+            string path = Path.Combine(System.IO.Path.GetTempPath() + @"\", "inputFile.txt");
             ImportService importService = new ImportService(path);
 
             Assert.AreEqual<string>("Name: Brad Howard", importService.ReadName());
+
+            importService.Dispose();
         }
 
         [TestMethod]
         public void ParseName_FirstNameFirst()
         {
-            string path = Path.Combine(System.Environment.CurrentDirectory + @"\..\..\..\", "inputFile.txt");
+            string path = Path.Combine(System.IO.Path.GetTempPath() + @"\", "inputFile.txt");
             ImportService importService = new ImportService(path);
 
             Assert.AreEqual<string>("Brad", importService.ParseName(importService.ReadName())[0]);
+
+            importService.Dispose();
         }
 
         [TestMethod]
         public void ParseName_LastNameFirst()
         {
-            string path = Path.Combine(System.Environment.CurrentDirectory + @"\..\..\..\", "inputFile2.txt");
+            string path = Path.Combine(System.IO.Path.GetTempPath() + @"\", "inputFile2.txt");
             ImportService importService = new ImportService(path);
 
             Assert.AreEqual<string>("Brad", importService.ParseName(importService.ReadName())[0]);
+
+            importService.Dispose();
         }
 
         [TestMethod]
         [ExpectedException(typeof(FormatException))]
         public void ParseName_MissingNameAfterColon()
         {
-            string path = Path.Combine(System.Environment.CurrentDirectory + @"\..\..\..\", "inputFile3.txt");
+            string path = Path.Combine(System.IO.Path.GetTempPath() + @"\", "inputFile3.txt");
             ImportService importService = new ImportService(path);
 
             Assert.AreEqual<string>("Brad", importService.ParseName(importService.ReadName())[0]);
+
+            importService.Dispose();
         }
 
         [TestMethod]
         [ExpectedException(typeof(FormatException))]
         public void PasrseName_MalformedInput()
         {
-            string path = Path.Combine(System.Environment.CurrentDirectory + @"\..\..\..\", "inputFile4.txt");
+            string path = Path.Combine(System.IO.Path.GetTempPath() + @"\", "inputFile4.txt");
             ImportService importService = new ImportService(path);
 
             Assert.AreEqual<string>("Brad", importService.ParseName(importService.ReadName())[0]);
+
+            importService.Dispose();
         }
 
         [TestMethod]
         [ExpectedException(typeof(FormatException))]
         public void PasrseName_MissingLastName()
         {
-            string path = Path.Combine(System.Environment.CurrentDirectory + @"\..\..\..\", "inputFile5.txt");
+            string path = Path.Combine(System.IO.Path.GetTempPath() + @"\", "inputFile5.txt");
             ImportService importService = new ImportService(path);
 
             Assert.AreEqual<string>("Brad", importService.ParseName(importService.ReadName())[0]);
+
+            importService.Dispose();
         }
 
         [TestMethod]
         [ExpectedException(typeof(FormatException))]
         public void ParseName_MissingFirstName()
         {
-            string path = Path.Combine(System.Environment.CurrentDirectory + @"\..\..\..\", "inputFile6.txt");
+            string path = Path.Combine(System.IO.Path.GetTempPath() + @"\", "inputFile6.txt");
             ImportService importService = new ImportService(path);
 
             Assert.AreEqual<string>("Brad", importService.ParseName(importService.ReadName())[0]);
+
+            importService.Dispose();
         }
 
         [TestMethod]
         [ExpectedException(typeof(FileNotFoundException))]
         public void ReadFileForImport_FileNotFound()
         {
-            string path = Path.Combine(System.Environment.CurrentDirectory + @"\..\..\..\", "inputFile8.txt");
+            string path = Path.Combine(System.IO.Path.GetTempPath() + @"\", "inputFile8.txt");
             ImportService importService = new ImportService(path);
 
             Assert.AreEqual<string>("Brad", importService.ParseName(importService.ReadName())[0]);
+
+            importService.Dispose();
         }
 
         [TestMethod]
         public void Dispose_Called()
         {
-            string path = Path.Combine(System.Environment.CurrentDirectory + @"\..\..\..\", "inputFile.txt");
+            string path = Path.Combine(System.IO.Path.GetTempPath() + @"\", "inputFile.txt");
             ImportService importService;
 
             using (importService = new ImportService(path))
