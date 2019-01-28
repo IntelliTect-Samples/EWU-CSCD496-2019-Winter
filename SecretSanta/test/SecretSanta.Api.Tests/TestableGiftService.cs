d30eacfa2f68191a122009c9ee63567cef24bd31
Brad Howard <bhoward4@ewu.edu>
2019-01-27 19:43:20 -0800

    Minor Fix
    Fixed code to insure no compile errors.

--- SecretSanta/test/SecretSanta.Api.Tests/TestableGiftService.cs
+++ SecretSanta/test/SecretSanta.Api.Tests/TestableGiftService.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using SecretSanta.Domain.Services;
 using SecretSanta.Domain.Models;
+using SecretSanta.Domain.Interfaces;
 
 namespace SecretSanta.Api.Tests
 {
@@ -8,6 +9,21 @@ namespace SecretSanta.Api.Tests
     {
         public List<Gift> ToReturn { get; set; }
         public int GetGiftsForUser_UserId { get; set; }
+
+        public void CreateGift(User user, Gift gift)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DeleteGift(User user, Gift gift)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void EditGift(User user, Gift gift)
+        {
+            throw new System.NotImplementedException();
+        }
 
         public List<Gift> GetGiftsForUser(int userId)
         {
