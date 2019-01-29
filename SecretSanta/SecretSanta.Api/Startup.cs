d17c31c74aec60b5e7fa6d5b6d68fa1c5d7bb7c1
Chris <raeder929@hotmail.com>
2019-01-29 13:40:08 -0800

    Initial setup, going to do a lot today, was quite busy over the weekend.

---
+++ SecretSanta/SecretSanta.Api/Startup.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SecretSanta.Domain.Models;
+using SecretSanta.Domain.Services;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace SecretSanta.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            var connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
+            services.AddDbContext<SecretSantaDbContext>(builder =>
+            {
+                builder.UseSqlite(connection);
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
