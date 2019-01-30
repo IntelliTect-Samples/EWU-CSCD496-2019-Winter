fc1110de41ebe40ec19ff35000be9e087ea45a8a
Chris <raeder929@hotmail.com>
2019-01-29 19:08:14 -0800

    added remainder of example code, just need to expand on this for all other parts

---
+++ SecretSanta/SecretSanta.Api/DTO/Gift.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SecretSanta.Api.DTO
+{
+    public class Gift
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int OrderOfImportance { get; set; }
+        public string URL { get; set; }
+        public string Description { get; set; }
+
+        public Gift()
+        {
+
+        }
+
+        public Gift(SecretSanta.Domain.Models.Gift gift)
+        {
+            if (gift == null) throw new ArgumentNullException(nameof(gift));
+
+            Id = gift.Id;
+            Title = gift.Title;
+            OrderOfImportance = gift.OrderOfImportance;
+            URL = gift.URL;
+            Description = gift.Description;
+        }
+    }
+}
