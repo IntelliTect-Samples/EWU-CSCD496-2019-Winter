f73ee699eea3e203686e595900e5ba62f96dac40
Cameron Osborn <cameron0401@gmail.com>
2019-01-31 02:11:32 -0800

    Expand GroupService and Tests
    Add RemoveUserFromGroup, AddUserToGroup, and DeleteGroup to GroupService
    Add simple accompanying unit tests to GroupService as basic validation.

--- SecretSanta/src/SecretSanta.Domain/Services/GroupService.cs
+++ SecretSanta/src/SecretSanta.Domain/Services/GroupService.cs
@@ -2,6 +2,7 @@ using SecretSanta.Domain.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace SecretSanta.Domain.Services
 {
@@ -40,6 +41,54 @@ namespace SecretSanta.Domain.Services
                 .SelectMany(x => x.GroupUsers)
                 .Select(x => x.User)
                 .ToList();
+        }
+
+        public void DeleteGroup(Group group)
+        {
+            if (@group == null) throw new ArgumentNullException(nameof(@group));
+
+            DbContext.Groups.Remove(group);
+            DbContext.SaveChanges();
+        }
+
+        public User AddUserToGroup(int groupId, User user)
+        {
+            if (user == null) throw new ArgumentNullException(nameof(user));
+            
+            if (groupId <= 0) throw new ArgumentException($"Invalid groupId. {groupId} must be greater than 0");
+
+            Group groupInsertingInto = DbContext.Groups
+                .Include(group => group.GroupUsers)
+                .SingleOrDefault(group => group.Id == groupId);
+
+            groupInsertingInto?.GroupUsers.Add(new GroupUser
+            {
+                GroupId = groupId,
+                UserId = user.Id,
+                User = user
+            });
+
+            DbContext.SaveChanges();
+
+            return user;
+        }
+
+        public User RemoveUserFromGroup(int groupId, User user)
+        {
+            if (user == null) throw new ArgumentNullException(nameof(user));
+            
+            if (groupId <= 0) throw new ArgumentException($"Invalid groupId. {groupId} must be greater than 0");
+
+            Group groupRemovingFrom = DbContext.Groups
+                .Include(group => group.GroupUsers)
+                .SingleOrDefault(group => group.Id == groupId);
+
+            groupRemovingFrom?.GroupUsers
+                .Remove(groupRemovingFrom?.GroupUsers
+                    .SingleOrDefault(groupUser => groupUser.GroupId == groupId 
+                                                  && groupUser.UserId == user.Id));
+
+            return user;
         }
     }
 }
--- SecretSanta/src/SecretSanta.Domain/Services/IGroupService.cs
+++ SecretSanta/src/SecretSanta.Domain/Services/IGroupService.cs
@@ -8,5 +8,9 @@ namespace SecretSanta.Domain.Services
         Group AddGroup(Group group);
         Group UpdateGroup(Group group);
         List<Group> FetchAll();
+        List<User> GetUsers(int groupId);
+        void DeleteGroup(Group group);
+        User AddUserToGroup(int groupId, User user);
+        User RemoveUserFromGroup(int groupId, User user);
     }
 }
