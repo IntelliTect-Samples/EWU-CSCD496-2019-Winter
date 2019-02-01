dbb1acbccb89df03772dee368944fe2914e33797
Isaac <ibliss@eagles.ewu.edu>
2019-01-31 23:25:38 -0800

    Add the beginnings of testing

--- SecretSanta/test/SecretSanta.Api.Tests/GroupControllerTests.cs
+++ SecretSanta/test/SecretSanta.Api.Tests/GroupControllerTests.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SecretSanta.Api.Controllers;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,5 +10,64 @@ namespace SecretSanta.Api.Tests
     [TestClass]
     public class GroupControllerTests
     {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void UserController_RequiresGiftService()
+        {
+            new GroupController(null);
+        }
+
+        [TestMethod]
+        public void AddGroup_NullGroup()
+        {
+            var testService = new TestableGroupService();
+            var controller = new GroupController(testService);
+
+            ActionResult result = controller.AddGroup(null);
+
+            Assert.IsTrue(result is BadRequestResult);
+        }
+
+        [TestMethod]
+        public void UpdateGroup_NullGroup()
+        {
+            var testService = new TestableGroupService();
+            var controller = new GroupController(testService);
+
+            ActionResult result = controller.UpdateGroup(null);
+
+            Assert.IsTrue(result is BadRequestResult);
+        }
+
+        [TestMethod]
+        public void RemoveGroup_NullGroup()
+        {
+            var testService = new TestableGroupService();
+            var controller = new GroupController(testService);
+
+            ActionResult result = controller.RemoveGroup(null);
+
+            Assert.IsTrue(result is BadRequestResult);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void FetchGroupUsers_NoValidGroupToSearch()
+        {
+            var testService = new TestableGroupService();
+            var controller = new GroupController(testService);
+
+            ActionResult<List<DTO.User>> result = controller.FetchGroupUsers(0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void FetchAll_NoGroups()
+        {
+            var testService = new TestableGroupService();
+            var controller = new GroupController(testService);
+
+            ActionResult<List<DTO.Group>> result = controller.FetchAll();
+        }
     }
 }
--- SecretSanta/test/SecretSanta.Api.Tests/UserControllerTests.cs
+++ SecretSanta/test/SecretSanta.Api.Tests/UserControllerTests.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SecretSanta.Api.Controllers;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,5 +10,54 @@ namespace SecretSanta.Api.Tests
     [TestClass]
     public class UserControllerTests
     {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void UserController_RequiresGiftService()
+        {
+            new UserController(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void FetchAll_NoUsers()
+        {
+            var testService = new TestableUserService();
+            var controller = new UserController(testService);
+
+            ActionResult<List<DTO.User>> result = controller.FetchAll();
+        }
+
+        [TestMethod]
+        public void AddUser_NullUser()
+        {
+            var testService = new TestableUserService();
+            var controller = new UserController(testService);
+
+            ActionResult result = controller.AddUser(null);
+
+            Assert.IsTrue(result is BadRequestResult);
+        }
+
+        [TestMethod]
+        public void UpdateUser_NullUser()
+        {
+            var testService = new TestableUserService();
+            var controller = new UserController(testService);
+
+            ActionResult result = controller.UpdateUser(null);
+
+            Assert.IsTrue(result is BadRequestResult);
+        }
+
+        [TestMethod]
+        public void RemoveUser_NullUser()
+        {
+            var testService = new TestableUserService();
+            var controller = new UserController(testService);
+
+            ActionResult result = controller.RemoveUser(null);
+
+            Assert.IsTrue(result is BadRequestResult);
+        }
     }
 }
