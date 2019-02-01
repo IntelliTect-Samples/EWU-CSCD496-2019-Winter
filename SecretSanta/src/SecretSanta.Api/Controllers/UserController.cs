57f58d6e135105bafbe47df991dd5cc26ea8cf0e
Isaac <ibliss@eagles.ewu.edu>
2019-01-31 19:47:27 -0800

    skeleton for user functions
    Added delete user to userservice. Added Iuserservice, user DTO, usercontroler, and altered startup

---
+++ SecretSanta/src/SecretSanta.Api/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SecretSanta.Domain.Services;
+
+namespace SecretSanta.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _UserService;
+
+        public UserController(IUserService userService)
+        {
+            _UserService = userService ?? throw new ArgumentNullException(nameof(userService));
+        }
+    }
+}
---
+++ SecretSanta/src/SecretSanta.Domain/Services/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SecretSanta.Domain.Models;
+
+namespace SecretSanta.Domain.Services
+{
+    public interface IUserService
+    {
+        User AddUser(User user);
+        
+        User UpdateUser(User user);
+
+        void RemoveUser(User user);
+
+        List<User> FetchAll();
+    }
+}
