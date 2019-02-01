d46c2acbffb1773dbb2a82cf8e5864e5adc8768c
Isaac <ibliss@eagles.ewu.edu>
2019-01-31 22:07:13 -0800

    added the testable services for user and group

---
+++ SecretSanta/test/SecretSanta.Api.Tests/TestableGroupService.cs
@@ -0,0 +1,46 @@
+﻿using SecretSanta.Domain.Models;
+using SecretSanta.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretSanta.Api.Tests
+{
+    class TestableGroupService : IGroupService
+    {
+        public Group AddGroup_Group { get; set; }
+        public Group ToReturnGroup { get; set; }
+        public Group AddGroup(Group group)
+        {
+            AddGroup_Group = group;
+            return ToReturnGroup;
+        }
+
+        public List<Group> ToReturnGroupList { get; set; }
+        public List<Group> FetchAll()
+        {
+            return ToReturnGroupList;
+        }
+
+        public int FetchAllUsersInGroup_GroupId { get; set; }
+        public List<User> ToReturnUserList { get; set; }
+        public List<User> FetchGroupUsers(int groupId)
+        {
+            FetchAllUsersInGroup_GroupId = groupId;
+            return ToReturnUserList;
+        }
+
+        public Group RemoveGroup_Group { get; set; }
+        public void RemoveGroup(Group group)
+        {
+            RemoveGroup_Group = group;
+        }
+
+        public Group UpdateGroup_Group { get; set; }
+        public Group UpdateGroup(Group group)
+        {
+            UpdateGroup_Group = group;
+            return ToReturnGroup;
+        }
+    }
+}
---
+++ SecretSanta/test/SecretSanta.Api.Tests/TestableUserService.cs
@@ -0,0 +1,38 @@
+﻿using SecretSanta.Domain.Models;
+using SecretSanta.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretSanta.Api.Tests
+{
+    class TestableUserService : IUserService
+    {
+        public User AddUser_User { get; set; }
+        public User ToReturn { get; set; }
+        public User AddUser(User user)
+        {
+            AddUser_User = user;
+            return ToReturn;
+        }
+
+        public List<User> ToReturnList { get; set; }
+        public List<User> FetchAll()
+        {
+            return ToReturnList;
+        }
+
+        public User RemoveUser_User { get; set; }
+        public void RemoveUser(User user)
+        {
+            RemoveUser_User = user;
+        }
+
+        public User UpdateUser_User { get; set; }
+        public User UpdateUser(User user)
+        {
+            UpdateUser_User = user;
+            return ToReturn;
+        }
+    }
+}
