0a8ee23e51f1169c774635ef168554d4d0c68fa5
Unknown <rbrintont823@live.com>
2019-01-31 21:37:49 -0800

    Decided not to go with Moq, implemented TestableGiftService

---
+++ SecretSanta/test/SecretSanta.Api.Tests/TestableGiftService.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using SecretSanta.Domain.Services;
+using SecretSanta.Domain.Models;
+
+namespace SecretSanta.Api.Tests
+{
+    public class TestableGiftService : IGiftService
+    {
+        public List<Gift> GiftsToReturn { get; set; }
+        public int LastModifiedUserId { get; set; }
+        public Gift LastModifiedGift { get; set; }
+
+        public List<Gift> GetGiftsForUser(int userId)
+        {
+            LastModifiedUserId = userId;
+            return GiftsToReturn;
+        }
+
+        public Gift AddGiftToUser(int userId, Gift gift)
+        {
+            LastModifiedUserId = userId;
+            LastModifiedGift = gift;
+            return gift;
+        }
+
+        public Gift UpdateGiftForUser(int userId, Gift gift)
+        {
+            LastModifiedUserId = userId;
+            LastModifiedGift = gift;
+            return gift;
+        }
+
+        public void RemoveGift(Gift gift)
+        {
+            LastModifiedGift = gift;
+        }
+    }
+}
