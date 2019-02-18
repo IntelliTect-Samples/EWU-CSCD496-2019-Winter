f95f7dbe4670d304cd438a31ce4f2a5fe4e7a73f
Cameron Osborn <cameron0401@gmail.com>
2019-02-17 19:20:04 -0800

    Add method to SeedDatabase to cut down on repeated testing code.

--- SecretSanta/test/SecretSanta.Domain.Tests/Services/PairingServiceTests.cs
+++ SecretSanta/test/SecretSanta.Domain.Tests/Services/PairingServiceTests.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.Extensions.Options;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SecretSanta.Domain.Models;
@@ -7,37 +8,61 @@ using SecretSanta.Domain.Services;
 namespace SecretSanta.Domain.Tests.Services
 {
     [TestClass]
-    public class PairingServiceTests : DatabaseServiceTests // TODO:
+    public class PairingServiceTests : DatabaseServiceTests
     {
-        [TestMethod]
-        public void GeneratePairing_Success()
+        [TestInitialize]
+        public void Initialize()
         {
-            TestableRandom testableRandom = new TestableRandom {RandomList = new List<int> {5}};
+            SeedDatabase(3);
+        }
+
+        [TestMethod]
+        public async Task PairingService_GeneratePairings_Success()
+        {
             using (var context = new ApplicationDbContext(Options))
             {
-                PairingService pairingService = new PairingService(context, testableRandom);
-                UserService userService = new UserService(context);
-                GroupService groupService = new GroupService(context);
+                var pairingService = new PairingService(context);
+                TestableRandom random = new TestableRandom { RandomList = new List<int>{5}};
+                var result = await pairingService.GeneratePairings(1);
                 
-                var user = new User
-                {
-                    FirstName = "Inigo",
-                    LastName = "Montoya"
-                };
+                Assert.AreEqual(3, result.Count);
+                // Ensure that a loop is created
+                Assert.AreEqual(result[0].SantaId, result[2].RecipientId);
+            }
+        }
 
-                var user2 = new User
-                {
-                    FirstName = "Princess",
-                    LastName = "Buttercup"
-                };
+        private static int _groupCount = 0; // ensures that the users/group name are unique. Keeps count of groups seeded
+        private async void SeedDatabase(int userCount)
+        {
+            using (var context = new ApplicationDbContext(Options))
+            {
+                var userService = new UserService(context);
+                
+                List<User> usersAdded = new List<User>();
 
-                var user3 = new User
+                for (int i = 0; i < userCount; i++)
                 {
-                    FirstName = "Someone",
-                    LastName = "Else"
+                    User user = new User
+                    {
+                        FirstName = $"User{i}-{++_groupCount}",
+                        LastName = $"User{i}-{++_groupCount}"
+                    };
+                    usersAdded.Add(user);
+                    await userService.AddUser(user);
+                }
+                
+                var group = new Group
+                {
+                    Name = $"Group-{_groupCount}"
                 };
                 
-                List<User> users = new List<User>{user, user2, user3};
+                var groupService = new GroupService(context);
+                Group addedGroup = await groupService.AddGroup(group);
+
+                foreach (var curUser in usersAdded)
+                {
+                    await groupService.AddUserToGroup(addedGroup.Id, curUser.Id);
+                }
             }
         }
     }
