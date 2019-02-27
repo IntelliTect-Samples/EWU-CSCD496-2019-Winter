87472f088fbbc697f99ffe22f79e49e155492cf4
Cameron Osborn <cameron0401@gmail.com>
2019-02-27 15:44:30 -0800

    Add Group Index
    Display Groups from Database on Group Index.

--- SecretSanta/src/SecretSanta.Web/Controllers/GroupsController.cs
+++ SecretSanta/src/SecretSanta.Web/Controllers/GroupsController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SecretSanta.Web.ApiModels;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,9 +12,18 @@ namespace SecretSanta.Web.Controllers
 {
     public class GroupsController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+        private IHttpClientFactory ClientFactory { get; }
+        public GroupsController(IHttpClientFactory clientFactory)
         {
+            ClientFactory = clientFactory;
+        }
+        public async Task<IActionResult> Index()
+        {
+            using (var httpClient = ClientFactory.CreateClient("SecretSantaApi"))
+            {
+                var secretSantaClient = new SecretSantaClient(httpClient.BaseAddress.ToString(), httpClient);
+                ViewBag.Groups = await secretSantaClient.GetGroupsAsync();
+            }
             return View();
         }
     }
