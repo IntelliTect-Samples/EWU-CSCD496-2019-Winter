9f9e3d51403308c59aefc9969b0709d3b785f97b
Isaac <ibliss@eagles.ewu.edu>
2019-02-28 20:23:40 -0800

    Nswagstudio generated class added and able to display all users

---
+++ SecretSanta/src/SecretSanta.Web/Controllers/UsersController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SecretSanta.Web.ApiModels;
+//using SecretSanta.Web.ViewModels;
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SecretSanta.Web.Controllers
+{
+    public class UsersController : Controller
+    {
+        private IHttpClientFactory ClientFactory { get; }
+        public UsersController(IHttpClientFactory clientFactory)
+        {
+            ClientFactory = clientFactory;
+        }
+
+        // GET: /<controller>/
+        public async Task<IActionResult> Index()
+        {
+            using (var httpClient = ClientFactory.CreateClient("SecretSantaApi"))
+            {
+                SecretSantaClient secretSantaClient = new SecretSantaClient(httpClient.BaseAddress.ToString(), httpClient);
+                ViewBag.Users = await secretSantaClient.GetAllUsersAsync();
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Add(UserInputViewModel viewModel)
+        {
+            IActionResult result = View();
+
+            if (ModelState.IsValid)
+            {
+                using (var httpClient = ClientFactory.CreateClient("SecretSantaApi"))
+                {
+                    var response = await httpClient.PostAsJsonAsync<UserInputViewModel>("/api/users", viewModel);
+                    ViewBag.StatusCode = response.StatusCode;
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        result = RedirectToAction(nameof(Index));
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
