8927058361834edba17b253280c50468c7aca1ca
Cameron Osborn <cameron0401@gmail.com>
2019-03-07 20:26:42 -0800

    Finalize error suppression.

--- SecretSanta/src/SecretSanta.Api/GlobalSuppressions.cs
+++ SecretSanta/src/SecretSanta.Api/GlobalSuppressions.cs
@@ -6,3 +6,6 @@
 
 [assembly: System.CLSCompliant(false)] // Migration is not CLS compliant
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "CA2007:Do not directly await a Task", Justification = "ASP.NET core does not have a synchronization context")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1721:Property names should not match get methods", Justification = "Arguably in this circumstance this improves readability. The Get verb is necessary for clean API coding suggestions.", Scope = "member", Target = "~M:SecretSanta.Api.Controllers.UsersController.GetUser(System.Int32)~System.Threading.Tasks.Task{Microsoft.AspNetCore.Mvc.ActionResult{SecretSanta.Api.ViewModels.UserViewModel}}")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1060:Move pinvokes to native methods class", Justification = "This is a simple helper file. In the future it may be wise to extract this and similar calls to a NativeMethods class", Scope = "type", Target = "~T:SecretSanta.Api.Models.CurrentDirectoryHelpers")]
+
--- SecretSanta/src/SecretSanta.Api/ViewModels/GiftInputViewModel.cs
+++ SecretSanta/src/SecretSanta.Api/ViewModels/GiftInputViewModel.cs
@@ -12,7 +12,7 @@ namespace SecretSanta.Api.ViewModels
         public string Title { get; set; }
         public string Description { get; set; }
         public int? OrderOfImportance { get; set; }
-        public string Url { get; set; }
+        public Uri Url { get; set; }
         public int UserId { get; set; }
     }
 }
