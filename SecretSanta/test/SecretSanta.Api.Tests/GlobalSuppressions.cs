c2ecc26630124003a9574959db17583b490574fc
Cameron Osborn <cameron0401@gmail.com>
2019-03-07 19:54:21 -0800

    Add to suppression file, remove unused tests
    Added ignoring of read-only warning to List property on Testable Service

--- SecretSanta/test/SecretSanta.Api.Tests/GlobalSuppressions.cs
+++ SecretSanta/test/SecretSanta.Api.Tests/GlobalSuppressions.cs
@@ -7,4 +7,5 @@
 [assembly: System.CLSCompliant(false)] // Factories not CLS compliant
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "CA2007:Do not directly await a Task", Justification = "ASP.NET core does not have a synchronization context")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "Underscores are used in these Unit Tests to provide additional clarity")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "This is a simple property used to allow abstraction for testing. It is ok to allow overwriting of the entire list.", Scope = "member", Target = "~P:SecretSanta.Api.Tests.TestableGiftService.ToReturn")]
 
