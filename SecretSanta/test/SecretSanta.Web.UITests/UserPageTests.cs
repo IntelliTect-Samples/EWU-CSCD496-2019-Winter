57ba622a58e716203f2227cf3a11eda86358bbcb
Cameron Osborn <cameron0401@gmail.com>
2019-03-13 01:45:18 -0700

    Refactor tests with custom UITester. Add updated screenshot
    UITester wraps a unit test allowing for multiple drivers to be used
    Still need to setup an additional WebDriver to ensure functionality

--- SecretSanta/test/SecretSanta.Web.UITests/UserPageTests.cs
+++ SecretSanta/test/SecretSanta.Web.UITests/UserPageTests.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
@@ -12,137 +11,142 @@ namespace SecretSanta.Web.UITests
     public class UserPageTests
     {
         private const string RootUrl = "https://localhost:44308/";
-        public const string ScreenshotFolderName = "Screenshots";
-        private IWebDriver Driver { get; set; }
         public TestContext TestContext { get; set; }
+
+        public UITester Tester { get; set; }
 
         [TestInitialize]
         public void Init()
         {
-            Driver = new ChromeDriver(Path.GetFullPath("."));
-        }
+            IWebDriver ChromeWebDriver = new ChromeDriver(Path.GetFullPath("."));
 
-        [TestCleanup]
-        public void Cleanup()
-        {
-            if (TestContext.CurrentTestOutcome == UnitTestOutcome.Failed)
-            {
-                string fileName = $"{TestContext.TestName}.png";
-                string currentDirectory = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
-                string path = Path.Combine(currentDirectory, ScreenshotFolderName, fileName);
+            List<IWebDriver> drivers = new List<IWebDriver>{ChromeWebDriver};
 
-                Screenshot screenshot = ((ITakesScreenshot) Driver).GetScreenshot();
-                screenshot.SaveAsFile(path);
-            }
-
-            Driver.Quit();
-            Driver.Dispose();
+            Tester = new UITester(drivers);
         }
 
         [TestMethod]
         public void CanNavigateToUsersPage()
         {
-            Driver.Navigate().GoToUrl(RootUrl);
+            Tester.UITest((Driver) =>
+            {
+                Driver.Navigate().GoToUrl(RootUrl);
 
-            var homePage = new HomePage(Driver);
+                var homePage = new HomePage(Driver);
 
-            homePage.UsersLink.Click();
+                homePage.UsersLink.Click();
 
-            Assert.IsTrue(Driver.Url.EndsWith(UsersPage.Slug));
+                Assert.IsTrue(Driver.Url.EndsWith(UsersPage.Slug));
+            });
         }
 
         [TestMethod]
         public void CanNavigateToAddUsersPage()
         {
-            var rootUri = new Uri(RootUrl);
-            Driver.Navigate().GoToUrl(new Uri(rootUri, UsersPage.Slug));
-            var page = new UsersPage(Driver);
+            Tester.UITest((Driver) =>
+            {
+                var rootUri = new Uri(RootUrl);
+                Driver.Navigate().GoToUrl(new Uri(rootUri, UsersPage.Slug));
+                var page = new UsersPage(Driver);
 
-            page.AddUser.Click();
+                page.AddUser.Click();
 
-            Assert.IsTrue(Driver.Url.EndsWith(AddUsersPage.Slug));
+                Assert.IsTrue(Driver.Url.EndsWith(AddUsersPage.Slug));
+            });
         }
 
         [TestMethod]
         public void CanAddUsers()
         {
-            string userFirstName = "User Name" + Guid.NewGuid().ToString("N");
-            string userLastName = "Last Name" + Guid.NewGuid().ToString("N");
-            string fullName = userFirstName + " " + userLastName; // this is what we'll use to search with
-            UsersPage page = CreateUser(userFirstName, userLastName);
+            Tester.UITest((Driver) =>
+            {
+                string userFirstName = "User Name" + Guid.NewGuid().ToString("N");
+                string userLastName = "Last Name" + Guid.NewGuid().ToString("N");
+                string fullName = userFirstName + " " + userLastName; // this is what we'll use to search with
+                UsersPage page = CreateUser(Driver, userFirstName, userLastName);
 
-            Assert.IsTrue(Driver.Url.EndsWith(UsersPage.Slug));
-            List<string> userNames = page.UserNames;
-            Assert.IsTrue(userNames.Contains(fullName));
+                Assert.IsTrue(Driver.Url.EndsWith(UsersPage.Slug));
+                List<string> userNames = page.UserNames;
+                Assert.IsTrue(userNames.Contains(fullName));
+            });
         }
 
         [TestMethod]
         public void CanDeleteUser()
         {
-            string userFirstName = "User Name" + Guid.NewGuid().ToString("N");
-            string userLastName = "Last Name" + Guid.NewGuid().ToString("N");
-            string fullName = userFirstName + " " + userLastName; // this is what we'll use to search with
-            UsersPage page = CreateUser(userFirstName, userLastName);
+            Tester.UITest((Driver) =>
+            {
+                string userFirstName = "User Name" + Guid.NewGuid().ToString("N");
+                string userLastName = "Last Name" + Guid.NewGuid().ToString("N");
+                string fullName = userFirstName + " " + userLastName; // this is what we'll use to search with
+                UsersPage page = CreateUser(Driver, userFirstName, userLastName);
 
-            IWebElement deleteLink = page.GetDeleteLink(fullName);
-            deleteLink.Click();
-            Driver.SwitchTo().Alert().Accept();
+                IWebElement deleteLink = page.GetDeleteLink(fullName);
+                deleteLink.Click();
+                Driver.SwitchTo().Alert().Accept();
 
-            List<string> userNames = page.UserNames;
-            Assert.IsFalse(userNames.Contains(fullName));
+                List<string> userNames = page.UserNames;
+                Assert.IsFalse(userNames.Contains(fullName));
+            });
         }
 
         [TestMethod]
         public void CanEditUser()
         {
-            // Old user name
-            string oldUserFirstName = "First Name" + Guid.NewGuid().ToString("N");
-            string oldUserLastName = "Last Name" + Guid.NewGuid().ToString("N");
-            string oldFullName = oldUserFirstName + " " + oldUserLastName; // this is what we'll use to search with
+            Tester.UITest((Driver) =>
+            {
+                // Old user name
+                string oldUserFirstName = "First Name" + Guid.NewGuid().ToString("N");
+                string oldUserLastName = "Last Name" + Guid.NewGuid().ToString("N");
+                string oldFullName = oldUserFirstName + " " + oldUserLastName; // this is what we'll use to search with
 
-            // New user name
-            string newUserFirstName = "First Name" + Guid.NewGuid().ToString("N");
-            string newUserLastName = "Last Name" + Guid.NewGuid().ToString("N");
-            string newFullName = newUserFirstName + " " + newUserLastName;
+                // New user name
+                string newUserFirstName = "First Name" + Guid.NewGuid().ToString("N");
+                string newUserLastName = "Last Name" + Guid.NewGuid().ToString("N");
+                string newFullName = newUserFirstName + " " + newUserLastName;
 
-            UsersPage page = CreateUser(oldUserFirstName, oldUserLastName);
+                UsersPage page = CreateUser(Driver, oldUserFirstName, oldUserLastName);
 
-            // Make sure user was successfully added
-            Assert.IsTrue(Driver.Url.EndsWith(UsersPage.Slug));
-            List<string> userNames = page.UserNames;
-            Assert.IsTrue(userNames.Contains(oldFullName));
+                // Make sure user was successfully added
+                Assert.IsTrue(Driver.Url.EndsWith(UsersPage.Slug));
+                List<string> userNames = page.UserNames;
+                Assert.IsTrue(userNames.Contains(oldFullName));
 
-            // Attempt to edit user
-            IWebElement editLink = page.GetEditLink(oldFullName);
-            editLink.Click();
-            EditUsersPage editUsersPage = new EditUsersPage(Driver);
-            editUsersPage.UserFirstNameTextBox.Clear();
-            editUsersPage.UserFirstNameTextBox.SendKeys(newUserFirstName);
-            editUsersPage.UserLastNameTextBox.Clear();
-            editUsersPage.UserLastNameTextBox.SendKeys(newUserLastName);
-            editUsersPage.SubmitButton.Click();
+                // Attempt to edit user
+                IWebElement editLink = page.GetEditLink(oldFullName);
+                editLink.Click();
+                EditUsersPage editUsersPage = new EditUsersPage(Driver);
+                editUsersPage.UserFirstNameTextBox.Clear();
+                editUsersPage.UserFirstNameTextBox.SendKeys(newUserFirstName);
+                editUsersPage.UserLastNameTextBox.Clear();
+                editUsersPage.UserLastNameTextBox.SendKeys(newUserLastName);
+                editUsersPage.SubmitButton.Click();
 
-            // Ensure old username is removed and new user name exists
-            userNames = page.UserNames;
-            Assert.IsFalse(userNames.Contains(oldFullName));
-            Assert.IsTrue(userNames.Contains(newFullName));
+                // Ensure old username is removed and new user name exists
+                userNames = page.UserNames;
+                Assert.IsFalse(userNames.Contains(oldFullName));
+                Assert.IsTrue(userNames.Contains(newFullName));
+            });
         }
 
         [TestMethod]
         public void CanNavigateFromUsersToHome()
         {
-            var rootUri = new Uri(RootUrl);
-            Driver.Navigate().GoToUrl(new Uri(rootUri, UsersPage.Slug));
+            Tester.UITest((Driver) =>
+            {
+                var rootUri = new Uri(RootUrl);
+                Driver.Navigate().GoToUrl(new Uri(rootUri, UsersPage.Slug));
 
-            UsersPage usersPage = new UsersPage(Driver);
-            IWebElement homeLink = usersPage.HomeLink;
+                UsersPage usersPage = new UsersPage(Driver);
+                IWebElement homeLink = usersPage.HomeLink;
 
-            homeLink.Click();
+                homeLink.Click();
 
-            Assert.AreEqual(RootUrl, Driver.Url);
+                Assert.AreEqual(RootUrl, Driver.Url);
+            });
         }
 
-        private UsersPage CreateUser(string firstName, string lastName)
+        private UsersPage CreateUser(IWebDriver Driver, string firstName, string lastName)
         {
             var rootUri = new Uri(RootUrl);
             Driver.Navigate().GoToUrl(new Uri(rootUri, UsersPage.Slug));
@@ -155,6 +159,45 @@ namespace SecretSanta.Web.UITests
             addUserPage.UserLastNameTextBox.SendKeys(lastName);
             addUserPage.SubmitButton.Click();
             return page;
+        }
+    }
+
+    public class UITester
+    {
+        private IEnumerable<IWebDriver> Drivers;
+        private string FolderName { get; set; }
+        public UITester(IEnumerable<IWebDriver> drivers, string folderName = "Screenshots")
+        {
+            Drivers = drivers;
+            FolderName = folderName;
+        }
+
+        public void UITest(Action<IWebDriver> action)
+        {
+            foreach (IWebDriver webDriver in Drivers)
+            {
+                try
+                {
+                    action(webDriver);
+                }
+                catch (Exception ex)
+                {
+                    string fileName = $"{action.Method.Name}-{webDriver.GetType()}.png";
+                    fileName = string.Join("", fileName.Split(Path.GetInvalidFileNameChars()));
+                    string currentDirectory = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+                    string path = Path.Combine(currentDirectory, FolderName, fileName);
+
+                    Screenshot screenshot = ((ITakesScreenshot)webDriver).GetScreenshot();
+                    screenshot.SaveAsFile(path);
+
+                    throw;
+                }
+                finally
+                {
+                    webDriver.Quit();
+                    webDriver.Dispose();
+                }
+            }
         }
     }
 }
