7c8ea09e65c33720228e195391f5f112fc9587ee
Unknown <rbrintont823@live.com>
2019-03-14 12:31:06 -0700

    Added CancelLink property for testing

--- SecretSanta/test/SecretSanta.Web.UITests/EditUsersPage.cs
+++ SecretSanta/test/SecretSanta.Web.UITests/EditUsersPage.cs
@@ -7,13 +7,19 @@ namespace SecretSanta.Web.UITests
     public class EditUsersPage
     {
         public IWebDriver Driver { get; }
-        public IWebElement UserFirstNameTextBox => Driver.FindElement(By.Id("FirstName"));
-        public IWebElement UserLastNameTextBox => Driver.FindElement(By.Id("LastName"));
+        public IWebElement HomeLink => Driver.FindElement(By.CssSelector(".navbar-brand>a"));
+        public IWebElement UsersLink => Driver.FindElement(By.LinkText("Users"));
+        public IWebElement FirstNameTextBox => Driver.FindElement(By.Id("FirstName"));
+        public IWebElement LastNameTextBox => Driver.FindElement(By.Id("LastName"));
+        public string CurrentUserID =>
+            Driver.Url.Substring(Driver.Url.LastIndexOf("/") + 1);
 
         public IWebElement SubmitButton =>
             Driver
                 .FindElements(By.CssSelector("button.is-primary"))
                 .Single(x => x.Text == "Submit");
+
+        public IWebElement CancelLink => Driver.FindElement(By.LinkText("Cancel"));
 
         public EditUsersPage(IWebDriver driver)
         {
