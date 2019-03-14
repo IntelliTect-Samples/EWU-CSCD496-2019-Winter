910f86e8c36bc7f636ead866f589942f6417d036
Unknown <rbrintont823@live.com>
2019-03-13 21:37:52 -0700

    Page classes are stubbed out

---
+++ SecretSanta/test/SecretSanta.Web.UITests/HomePage.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium;
+using System;
+
+namespace SecretSanta.Web.UITests
+{
+    public class HomePage
+    {
+        public IWebDriver Driver { get; }
+
+        public UsersPage UsersPage => new UsersPage(Driver);
+        public IWebElement UsersLink => Driver.FindElement(By.LinkText("Users"));
+
+        public HomePage(IWebDriver driver)
+        {
+            Driver = driver ?? throw new ArgumentNullException(nameof(driver));
+        }
+    }
+}
---
+++ SecretSanta/test/SecretSanta.Web.UITests/UsersPage.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using OpenQA.Selenium;
+
+namespace SecretSanta.Web.UITests
+{
+    public class UsersPage
+    {
+        public IWebDriver Driver { get; set; }
+        public const string Slug = "Users";
+        public IWebElement AddUser => Driver.FindElement(By.LinkText("Add User"));
+        public IWebElement EditUser => Driver.FindElement(By.LinkText("Edit"));
+        public IWebElement DeleteUser => Driver.FindElement(By.LinkText("Delete"));
+        public IWebElement HomeLink => Driver.FindElement(By.CssSelector(".navbar-brand>a"));
+        public AddUsersPage AddUsersPage => new AddUsersPage(Driver);
+
+        public List<string> UserNames
+        {
+            get
+            {
+                var elements = Driver.FindElements(By.CssSelector("h1+ul>li"));
+
+                return elements
+                    .Select(x =>
+                    {
+                        var text = x.Text;
+                        if (text.EndsWith(" Edit Delete"))
+                        {
+                            text = text.Substring(0, text.Length - " Edit Delete".Length);
+                        }
+                        return text;
+                    })
+                    .ToList();
+            }
+        }
+
+        public IWebElement GetEditLink(string userName)
+        {
+            ReadOnlyCollection<IWebElement> editLinks =
+                Driver.FindElements(By.CssSelector("h1+ul>li"));
+
+            return editLinks.Single(x => x.Text == (userName + " Edit Delete")).FindElement(By.LinkText("Edit"));
+        }
+
+        public IWebElement GetDeleteLink(string userName)
+        {
+            ReadOnlyCollection<IWebElement> deleteLinks =
+                Driver.FindElements(By.CssSelector("a.is-danger"));
+
+            return deleteLinks.Single(x => x.GetAttribute("onclick").EndsWith($"{userName}?')"));
+        }
+
+        public UsersPage(IWebDriver driver)
+        {
+            Driver = driver ?? throw new ArgumentNullException(nameof(driver));
+        }
+    }
+}
